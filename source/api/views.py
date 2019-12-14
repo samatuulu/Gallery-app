417ac07718f709f11fd4772e66fc6d2fd8524b55
Bektursun <samatuulu11@gmail.com>
2019-12-14 11:12:02 +0600

    connected url patters to each apps

--- source/api/views.py
+++ source/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class LogoutView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        if user.is_authenticated:
+            user.auth_token.delete()
+        return Response({'status': 'ok'})
